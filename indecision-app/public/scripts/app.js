0c2d13d518e01dc4660c43442f7f44406a577177
Tatsuro <contacts@tatsuro.dev>
2020-02-13 17:06:39 +0900

    babel install, setup 9
    yarn global add babel-cli
    yarn init
    yarn add babel-preset-react@6.24.1 babel-preset-env@1.5.2
    babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
    live-server public

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,8 +1,13 @@
-console.log('App.js is running!')
+'use strict';
+
+console.log('App.js is running!');
 
 // JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>
-var template = React.createElement("h1", { id: "someid" }, "Something new")
-var appRoot = document.getElementById('app')
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
