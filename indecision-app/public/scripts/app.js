e908c1a523a87ca835a4dfc3c86e7fd178a0a92e
Tatsuro <contacts@tatsuro.dev>
2020-02-14 19:22:43 +0900

    attribute, event 16

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,45 +1,82 @@
 'use strict';
 
-// arrow functionでは、arguments objectにaccessできない
-var add = function add(a, b) {
-  // console.log(arguments)
-  return a + b;
+console.log('App.js is running!');
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-console.log(add(55, 1));
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
+);
 
-// this keyword
-var user = {
-  name: 'Andrew',
-  cities: ['Philadelphia', 'New York', 'Dublin'],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    // このmethodのthisはuser objectを指す
-    // console.log(this.name)
-    // console.log(this.cities)
-    // 無名関数内のthisは常にundefined
-    // arrow function内のthisはparentのthisと一緒
-    // this.cities.forEach((city) => {
-    //   // console.log(this)
-    //   console.log(this.name + ' has lived in ' + city)
-    // })
-    // mapは写像、forEachは何も返さない
-    return this.cities.map(function (city) {
-      return _this.name + ' has lived in ' + city;
-    });
-  }
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
 };
-console.log(user.printPlacesLived());
-
-var multiplier = {
-  numbers: [10, 20, 30],
-  multiplyBy: 3,
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var minusOne = function minusOne() {
+  console.log('minusOne');
 };
-console.log(multiplier.multiply());
+var reset = function reset() {
+  console.log('reset');
+};
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(templateTwo, appRoot);
