d51da7c86ffcee5fd3c1f96a6922927f8c56657f
Tatsuro <contacts@tatsuro.dev>
2020-04-05 05:54:46 +0900

    componentをnest 27

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 // componentの作成
 class Header extends React.Component {
   // render methodでjsxを返す
@@ -26,7 +39,18 @@ class Options extends React.Component {
     return (
       <div>
         Options component here
+        <Option />
+        {/* 上下は同値 */}
+        {/* <Option></Option> */}
       </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>Option component here</div>
     )
   }
 }
@@ -41,13 +65,4 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
-
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
