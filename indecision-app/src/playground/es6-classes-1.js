e58ef63084d2b5c029c0910e499d25e53fb5af4b
Tatsuro <contacts@tatsuro.dev>
2020-04-04 17:40:04 +0900

    es6 classの継承 25

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -16,8 +16,42 @@ class Person {
   }
 }
 
-const me = new Person('Andrew Mead', 26)
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age)
+    this.major = major
+  }
+
+  hasMajor() {
+    return !!this.major
+  }
+
+  getDescription() {
+    let description = super.getDescription()
+    if (description) {
+      description += ` Their major is ${this.major}.`
+    }
+    return description
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation) {
+    super(name, age)
+    this.homeLocation = homeLocation
+  }
+
+  getGreeting() {
+    let greeting = super.getGreeting()
+    if (this.homeLocation) {
+      greeting += ` I'm visiting from ${this.homeLocation}.`
+    }
+    return greeting
+  }
+}
+
+const me = new Traveler('Andrew Mead', 26, 'Philadelphia')
 console.log(me.getGreeting(), me.getDescription())
 
-const other = new Person()
+const other = new Traveler(undefined, undefined, 'Nowhere')
 console.log(other.getGreeting(), other.getDescription())
