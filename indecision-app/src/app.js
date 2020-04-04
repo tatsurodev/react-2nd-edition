bf72ed872988ba3e13f5ec826b76feecd8448fb1
Tatsuro <contacts@tatsuro.dev>
2020-04-05 05:10:58 +0900

    component作成 26

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,56 +1,53 @@
-console.log('App.js is running!')
-
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-}
-
-const onFormSubmit = (e) => {
-  e.preventDefault()
-  const option = e.target.elements.option.value
-  if (option) {
-    app.options.push(option)
-    e.target.elements.option.value = ''
-    render()
+// componentの作成
+class Header extends React.Component {
+  // render methodでjsxを返す
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    )
   }
 }
 
-const onRemoveAll = (e) => {
-  app.options = []
-  render()
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div >
+    )
+  }
 }
 
-const onMakeDecision = (e) => {
-  const randomNum = Math.floor(Math.random() * app.options.length)
-  const option = app.options[randomNum]
-  alert(option)
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        Options component here
+      </div>
+    )
+  }
 }
 
-const appRoot = document.getElementById('app')
-
-// jsx supports number, string, boolean, array, null, undefined, but not object
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {/* logical and operator */}
-      {app.subtitle && <p>{app.subtitle}</p>}
-      {/* ternary operator */}
-      <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
-      <button onClick={onMakeDecision} disabled={app.options.length === 0}>What should I do?</button>
-      <button onClick={onRemoveAll}>Remove All</button>
-      <ol>
-        {
-          app.options.map(option => <li key={option}>{option}</li>)
-        }
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button>Add Option</button>
-      </form>
-    </div>
-  )
-  ReactDOM.render(template, appRoot)
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        AddOption component here
+      </div>
+    )
+  }
 }
-render()
+
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
