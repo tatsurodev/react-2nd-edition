d268834c8884b207c17bfd1ac7563b7810f52af1
Tatsuro <contacts@tatsuro.dev>
2020-04-07 12:15:22 +0900

    eventでmethodをcall 29

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -30,19 +30,27 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick')
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        {/* eventですぐにfunctionをcallしたいわけではないので、括弧を付けずに括弧なしのreferenceをセットする */}
+        <button onClick={this.handlePick}>What should I do?</button>
       </div >
     )
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('handleRemoveAll')
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map(option => <Option key={option} optionText={option} />)
         }
@@ -62,10 +70,20 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault()
+    const option = e.target.elements.option.value.trim()
+    if (option) {
+      alert(option)
+    }
+  }
   render() {
     return (
       <div>
-        AddOption component here
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option</button>
+        </form>
       </div>
     )
   }
