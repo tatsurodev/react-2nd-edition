831b156fa46e89e3d358253ece1499acdd8b2248
Tatsuro <contacts@tatsuro.dev>
2020-04-17 04:03:22 +0900

    npm noduleをimport 53

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,14 +1,6 @@
-// importのみだと該当fileの変数はimportされないようになっている、むやみにimportされるとglobal scopeが汚染されてしまうため
-// import './utils.js'
-// exportと同じくimportする変数を指定
-// import subtract, { square, add } from './utils.js'
+// es6での使用例は、importを使う。from pathではなく、from 'moduleName'を使用すると、node_modules folderをwebpackが自動的に探しだす
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// console.log('app.js is running')
-// console.log(square(4))
-// console.log(add(100, 23))
-// console.log(subtract(100, 81))
-
-import isSenior, { isAdult, canDrink } from './person.js'
-console.log(isAdult(18))
-console.log(canDrink(20))
-console.log(isSenior(65))
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'))
