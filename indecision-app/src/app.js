1fde8c13bf534cc538caed35c8989362b3a2abe0
Tatsuro <contacts@tatsuro.dev>
2020-04-18 23:56:32 +0900

    IndecisionAppを各componentに分割 55

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,6 +1,6 @@
 // es6での使用例は、importを使う。from pathではなく、from 'moduleName'を使用すると、node_modules folderをwebpackが自動的に探しだす
 import React from 'react'
 import ReactDOM from 'react-dom'
+import IndecisionApp from './component/IndecisionApp'
 
-const template = <p>THIS IS JSX FROM WEBPACK</p>
-ReactDOM.render(template, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
---
+++ indecision-app/src/component/IndecisionApp.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import Header from './Header'
+import Action from './Action'
+import AddOption from './AddOption'
+import Options from './Options'
+
+export default class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleDeleteOption = this.handleDeleteOption.bind(this)
+    this.state = {
+      options: []
+    }
+  }
+  // lifecycle methodはclass base componentのみ、stateless functional componentにはない機能
+  componentDidMount() {
+    // jsonの形式が間違っていたりするとエラーが出るので
+    try {
+      // localStorageに保存したものを取り出すには、string形式のものをparseする
+      const json = localStorage.getItem('options')
+      const options = JSON.parse(json)
+      if (options) {
+        this.setState(() => ({ options }))
+      }
+    } catch (e) {
+      // エラー時は何もしない、つまりdefaultのprops.options = []が使われる
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      // localStorageに保存するにはまずstringにしないとダメ
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem('options', json)
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount!')
+  }
+  // 親のstateを子要素から変更させる必要があるので、propsとしてstateを変更するfunctionを渡す
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }))
+  }
+  handleDeleteOption(optionToRemove) {
+    this.setState((prevState) => ({ options: prevState.options.filter((option) => option !== optionToRemove) }))
+    // console.log('hdo', optionToRemove)
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[Math.floor(randomNum)]
+    alert(option)
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
+    }
+    // pushは破壊的なのでprevStateで使わない
+    this.setState((prevState) => ({ options: prevState.options.concat([option]) }))
+  }
+  render() {
+    const subtitle = "Put your life in the hands of a computer"
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          // 親要素のstateを変更するhandleDeleteOptions functionを子要素にpropsとして渡す
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+      </div>
+    )
+  }
+}
