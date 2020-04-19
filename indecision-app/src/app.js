e5e74ca7d215c88bf5af3c8289c0dde300834a98
Tatsuro <contacts@tatsuro.dev>
2020-04-19 19:00:21 +0900

    webpackでscssをsetup 64
    yarn add style-loader css-loader sass-loader node-sass

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './component/IndecisionApp'
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
 
