cbbda6b9f9248de9515865e530c1de907a7504e2
Tatsuro <contacts@tatsuro.dev>
2020-04-19 22:30:09 +0900

    _settings.scssに変数定義、中央揃え用のcontainer util作成 67

--- indecision-app/src/component/Header.js
+++ indecision-app/src/component/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Header = (props) => (
   <div className="header">
-    {/* propsにaccess */}
-    <h1 className="header__title">{props.title}</h1>
-    {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    <div className="container">
+      {/* propsにaccess */}
+      <h1 className="header__title">{props.title}</h1>
+      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    </div>
   </div>
 )
 Header.defaultProps = {
--- indecision-app/src/component/IndecisionApp.js
+++ indecision-app/src/component/IndecisionApp.js
@@ -65,23 +65,25 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          // 親要素のstateを変更するhandleDeleteOptions functionを子要素にpropsとして渡す
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
+        <div className="container">
+          <Action
+            hasOptions={this.state.options.length > 0}
+            handlePick={this.handlePick}
+          />
+          <Options
+            options={this.state.options}
+            // 親要素のstateを変更するhandleDeleteOptions functionを子要素にpropsとして渡す
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption
+            handleAddOption={this.handleAddOption}
+          />
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
+        </div>
       </div>
     )
   }
