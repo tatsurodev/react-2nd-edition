eec4ba82fdb911bc8b8a5defea38977725ab0715
Tatsuro <contacts@tatsuro.dev>
2020-04-20 16:57:39 +0900

    options部のstyling 69

--- indecision-app/src/component/IndecisionApp.js
+++ indecision-app/src/component/IndecisionApp.js
@@ -70,20 +70,22 @@ export default class IndecisionApp extends React.Component {
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
-          <Options
-            options={this.state.options}
-            // 親要素のstateを変更するhandleDeleteOptions functionを子要素にpropsとして渡す
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteOption={this.handleDeleteOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
-          <OptionModal
-            selectedOption={this.state.selectedOption}
-            handleClearSelectedOption={this.handleClearSelectedOption}
-          />
+          <div className="widget">
+            <Options
+              options={this.state.options}
+              // 親要素のstateを変更するhandleDeleteOptions functionを子要素にpropsとして渡す
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
         </div>
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
+        />
       </div>
     )
   }
