ea40548a75ec18968c4e6eebfdd31e20e57dc695
Tatsuro <contacts@tatsuro.dev>
2020-04-22 02:27:18 +0900

    actionのdispatch 86

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,8 +1,44 @@
 import { createStore } from 'redux'
 
 // store作成時にdefaultのstateが必要になるので関数の引数のstateにdefault stateをset
-const store = createStore((state = { count: 0 }) => {
-  return state
+// 第二引数のactionによって処理内容をswitch
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    case 'RESET':
+      return {
+        count: 0
+      }
+    default:
+      return state
+
+  }
+})
+
+console.log(store.getState())
+
+// actoinをdispatch
+store.dispatch({
+  type: 'INCREMENT'
+})
+
+store.dispatch({
+  type: 'INCREMENT'
+})
+
+store.dispatch({
+  type: 'RESET'
+})
+
+store.dispatch({
+  type: 'DECREMENT'
 })
 
 console.log(store.getState())
