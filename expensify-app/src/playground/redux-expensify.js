3e018edccb407156457c2139e6189623fa7404ec
Tatsuro <contacts@tatsuro.dev>
2020-04-22 09:40:30 +0900

    複数のReducerをまとめてstoreを作成 92

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,49 @@
 import { createStore, combineReducers } from 'redux'
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// expenses reducer
+const expensesReducerDefaultState = []
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+// filters reducer
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+}
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+// storeの作成、demoStateのようなstateを複数のreducerを組み合わせて作成する
+const store = createStore(
+  // reducerをまとめる
+  combineReducers({
+    // keyとreducerのpair
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+)
+
+console.log(store.getState())
+
 const demoState = {
   expenses: [{
     id: 'fjdksafjlkdsajlk',
