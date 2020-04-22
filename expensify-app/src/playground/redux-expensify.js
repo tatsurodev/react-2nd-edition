68b6a867dbc93e97d89b89d34cdd438097caf0e4
Tatsuro <contacts@tatsuro.dev>
2020-04-22 20:32:21 +0900

    expenseのadd, remove用action generatorを作成, reducerに処理追加 93

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,32 @@
 import { createStore, combineReducers } from 'redux'
+import { v4 as uuid } from 'uuid'
 
 // ADD_EXPENSE
+const addExpense = (
+  {
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+  } = {}
+) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt
+  }
+})
+
 // REMOVE_EXPENSE
+// defaultを{}にすることで引数なしでもおｋ
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id
+})
+
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -13,6 +38,16 @@ import { createStore, combineReducers } from 'redux'
 const expensesReducerDefaultState = []
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case 'ADD_EXPENSE':
+      // state.pushは破壊的で処理後のlengthを返すので正常に処理できない、concatは配列に追加すして写像を返す
+      // return state.concat(action.expense)
+      // es6 spread operatorで配列に追加
+      return [
+        ...state,
+        action.expense
+      ]
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id)
     default:
       return state
   }
@@ -42,7 +77,16 @@ const store = createStore(
   })
 )
 
-console.log(store.getState())
+// stateのdebug用
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+// store.dispatchの返り値はaction object
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }))
+// expenseOneを削除
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 
 const demoState = {
   expenses: [{
