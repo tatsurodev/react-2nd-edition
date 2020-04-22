159161f5df6c93d8139f94ea9995a22d31152951
Tatsuro <contacts@tatsuro.dev>
2020-04-23 03:12:17 +0900

    state更新用のaction generatorとreducerを作成、更新の際にobject spread operatorを利用 94

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -28,7 +28,17 @@ const removeExpense = ({ id } = {}) => ({
 })
 
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+})
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -48,6 +58,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
       ]
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id)
+    case 'EDIT_EXPENSE':
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense
+        }
+      })
     default:
       return state
   }
@@ -62,6 +83,11 @@ const filtersReducerDefaultState = {
 }
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      }
     default:
       return state
   }
@@ -88,6 +114,12 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 // expenseOneを削除
 store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 
+// expenseの編集
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }))
+// filter系
+store.dispatch(setTextFilter('rent'))
+store.dispatch(setTextFilter())
+
 const demoState = {
   expenses: [{
     id: 'fjdksafjlkdsajlk',
@@ -103,3 +135,18 @@ const demoState = {
     endDate: undefined
   }
 }
+
+// object spread operator
+/*
+const user = {
+  name: 'Jen',
+  age: 24
+}
+
+console.log({
+  name: 'John', // not override
+  ...user,
+  location: 'Pholadelphia',
+  age: 27 // override
+})
+*/
