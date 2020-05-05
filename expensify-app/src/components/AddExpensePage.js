9df53ef0779721c8da81651e5fe00c8efd1f2ac9
Tatsuro <contacts@tatsuro.dev>
2020-05-06 04:13:52 +0900

    子のExpenseFormからexpense dataを親のAddExpensePageに渡してexpense追加 107

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
+import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = () => (
+const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
-    <ExpenseForm />
+    <ExpenseForm
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense))
+        props.history.push('/')
+      }}
+    />
   </div>
 )
 
-export default AddExpensePage
+// props.dispatchにaccessしたいだけで他のpropsにaccessしたいわけではないのでmapStateToPropsは不要
+export default connect()(AddExpensePage)
--- expensify-app/src/components/ExpenseForm.js
+++ expensify-app/src/components/ExpenseForm.js
@@ -14,7 +14,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   }
   onDescriptionChange = (e) => {
     const description = e.target.value
@@ -28,21 +29,43 @@ export default class ExpenseForm extends React.Component {
   }
   onAmountChange = (e) => {
     const amount = e.target.value
-    // validationを実行、正規表現で小数第2までの数字の時のみstate amountを更新
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    // validationを実行、正規表現で最低1桁で小数第2までの数字の時state amountを更新、amountない時にstate amountをclear
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }))
     }
   }
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }))
+    // clear防止
+    if (createdAt) {
+      this.setState(() => ({ createdAt }))
+    }
   }
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }))
   }
+  onSubmit = (e) => {
+    e.preventDefault()
+    // validation, stateのdescriptionとamountがなければ、error追加
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount' }))
+    } else {
+      // 正常時error clear
+      this.setState(() => ({ error: '' }))
+      // addExpensePage.jsのonSubmitでstate追加
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        // valueOf methodでmoment objectをtimestampに変換
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      })
+    }
+  }
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
