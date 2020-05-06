b6e2435c830e89e26ca4e21a9e4118999738a77e
Tatsuro <contacts@tatsuro.dev>
2020-05-06 15:33:23 +0900

    DateRangePickerでfiltering 110

--- expensify-app/src/selectors/expenses.js
+++ expensify-app/src/selectors/expenses.js
@@ -1,12 +1,14 @@
 // selectors folderの中にはreduxからstateを取得するためのquery処理を格納
+import moment from 'moment'
 // filterされたexpenseを取得
 export default (expenses, { text, sortBy, startDate, endDate }) => {
   // 3つの要素, text, startDate, endDateによってfilter
   // それぞれmatchするか判定、3つともtrueなら全体としてもtrueを返す
   return expenses.filter(expense => {
+    const createdAtMoment = moment(expense.createdAt)
     // timestamps(milliseconds): 基準日が1970-01-01 ex. 334000, 10, -203
-    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
-    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true
     // 大文字小文字関係なくmatchするかどうか
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
     return startDateMatch && endDateMatch && textMatch
