b925f8a08ba38dc304baf35fb40ebdf599903bc1
Tatsuro <contacts@tatsuro.dev>
2020-05-11 01:11:05 +0900

    dispatchをpropsにmap, test 124

--- expensify-app/src/tests/__mocks__/moment.js
+++ expensify-app/src/tests/__mocks__/moment.js
@@ -1,6 +1,8 @@
+// test時に本物のmomentをimportするとstack errorが出るので代わりにjest.requireActual
 // import moment from 'moment'
 const moment = jest.requireActual('moment')
 
+// test時にprops.expenseがある時はmoment(props.expense.createdAt)が、ない時はmoment(0)が実行され、props.expenseがない時にtimestamp=0で引数が固定されることによりsnapshotのerrorがなくなる
 export default (timestamp = 0) => {
   return moment(timestamp)
 }
