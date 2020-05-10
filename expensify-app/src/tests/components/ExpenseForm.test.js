461c44d49053e9843403f642c6e85f58b3d5840c
Tatsuro <contacts@tatsuro.dev>
2020-05-10 13:37:10 +0900

    spyを使ってtest 123

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
 import ExpenseForm from '../../components/ExpenseForm'
-import expeses from '../fixtures/expenses'
+import expenses from '../fixtures/expenses'
 
 test('should render ExpenseForm correctly', () => {
   const wrapper = shallow(<ExpenseForm />)
@@ -9,7 +10,7 @@ test('should render ExpenseForm correctly', () => {
 })
 
 test('should render ExpenseForm correctly with expense data', () => {
-  const wrapper = shallow(<ExpenseForm expense={expeses[1]} />)
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />)
   expect(wrapper).toMatchSnapshot()
 })
 
@@ -65,4 +66,35 @@ test('should not set amount if invalid input', () => {
     target: { value }
   })
   expect(wrapper.state('amount')).toBe('')
+})
+
+test('should call onSubmit prop for valid form submission', () => {
+  const onSubmitSpy = jest.fn()
+  const wrapper = shallow(<ExpenseForm expense={expenses[0]} onSubmit={onSubmitSpy} />)
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => { }
+  })
+  expect(wrapper.state('error')).toBe('')
+  // toHaveBeenCalledWith(expense[0])とかだとidがobjectに含まれてしまうので渡すobjectを作り直す
+  expect(onSubmitSpy).toHaveBeenCalledWith({
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    note: expenses[0].note,
+    createdAt: expenses[0].createdAt
+  })
+})
+
+test('should set new date on date change', () => {
+  const now = moment()
+  const wrapper = shallow(<ExpenseForm />)
+  // wrapper.find('SingleDatePicker').prop('onDateChange')(now)
+  wrapper.find('withStyles(SingleDatePicker)').prop('onDateChange')(now)
+  expect(wrapper.state('createdAt')).toEqual(now)
+})
+
+test('should set calendar focus on change', () => {
+  const focused = false
+  const wrapper = shallow(<ExpenseForm />)
+  wrapper.find('withStyles(SingleDatePicker)').prop('onFocusChange')({ focused })
+  expect(wrapper.state('calendarFocused')).toBe(focused)
 })
