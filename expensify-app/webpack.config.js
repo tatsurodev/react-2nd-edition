9aa9752ab19d8ca6e67a924b2e0787e2d5a1f512
Tatsuro <contacts@tatsuro.dev>
2020-05-15 17:18:10 +0900

    webpackにproduction用の設定を追加 133

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,32 +1,38 @@
 const path = require('path')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    // url直打ちで静的sourceが存在しない時404を返されるが、historyApiFallbackをtrueにするとtop urlを返した上でreact-routerによる変遷がなされる
-    historyApiFallback: true
+// objectではなく、関数を指定することによってenv等の引数の使用が可能になる
+module.exports = (env) => {
+  // webpack --evn productionのenv keyで得られる値によってdev or prodの判定を行う
+  const isProduction = env === 'production'
+  return {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }]
+    },
+    // prod時のみsource-mapでsource mapを別fileに
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      // url直打ちで静的sourceが存在しない時404を返されるが、historyApiFallbackをtrueにするとtop urlを返した上でreact-routerによる変遷がなされる
+      historyApiFallback: true
+    }
   }
 }
