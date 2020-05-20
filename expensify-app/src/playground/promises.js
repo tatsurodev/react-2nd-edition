80a07192978eb420f85421b709fcd8d8ac8a92b3
Tatsuro <contacts@tatsuro.dev>
2020-05-20 18:35:26 +0900

    async actionのtest 154

--- expensify-app/src/playground/promises.js
+++ expensify-app/src/playground/promises.js
@@ -17,7 +17,11 @@ console.log('before')
 promise.then(data => {
   console.log('1', data)
   // ここでreturnしたものが次のthenの引数として使える
-  return 'some data'
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('This is my other promise')
+    }, 1500)
+  })
 }).then((str) => {
   console.log('does this run?', str)
 }).catch(error => {
--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -3,6 +3,7 @@ import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
+import database from '../../firebase/firebase'
 
 // configureMockStoreの引数にはmiddlewareを指定
 const createMockStore = configureMockStore([thunk])
@@ -45,13 +46,43 @@ test('should add expense to database and store', (done) => {
   }
   // startAddExpenseはasynchronousなのでdoneでjestにtestが終わったことを知らせる必要がある
   store.dispatch(startAddExpense(expenseData)).then(() => {
-    expect(1).toBe(1)
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    })
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toEqual(expenseData)
     done()
   })
 })
 
-test('should add expense with defaults to database and store', () => {
-
+test('should add expense with defaults to database and store', (done) => {
+  const store = createMockStore({})
+  const expenseDefault = {
+    description: '',
+    amount: 0,
+    note: '',
+    createdAt: 0
+  }
+  store.dispatch(startAddExpense({})).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseDefault
+      }
+    })
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toEqual(expenseDefault)
+    done()
+  })
 })
 
 // test('should setup add expense action object with default values', () => {
