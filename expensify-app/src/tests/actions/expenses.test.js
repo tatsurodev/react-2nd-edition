2638d6c62816f742e6ca6acfb1710318cdf017aa
Tatsuro <contacts@tatsuro.dev>
2020-11-09 04:53:26 +0900

    SET_EXPENSESのaction, reducer, test作成 157

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -6,12 +6,25 @@ import {
   addExpense,
   editExpense,
   removeExpense,
+  setExpenses,
 } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
 
 // configureMockStoreの引数にはmiddlewareを指定
 const createMockStore = configureMockStore([thunk])
+
+beforeEach((done) => {
+  const expensesData = {}
+  expenses.forEach(({ id, description, note, amount, createdAt }) => {
+    expensesData[id] = { description, note, amount, createdAt }
+  })
+  // async処理なのでdoneで終わりを知らせないとdataをsetし終わる前にbeforeEachが終了してtestに入ってしまう
+  database
+    .ref('expenses')
+    .set(expensesData)
+    .then(() => done())
+})
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: '123abc' })
@@ -109,3 +122,11 @@ test('should add expense with defaults to database and store', (done) => {
 //     }
 //   })
 // })
+
+test('should setup set expense action object with data', () => {
+  const action = setExpenses(expenses)
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses,
+  })
+})
