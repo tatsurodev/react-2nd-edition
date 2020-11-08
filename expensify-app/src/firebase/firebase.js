3272f0cee2777fe6b87b7eeab3cba2bd483ff6ee
Tatsuro <contacts@tatsuro.dev>
2020-11-09 01:24:29 +0900

    test用databaseをsetup 155

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,15 +1,14 @@
 import * as firebase from 'firebase'
-const { FIREBASE_API_KEY } = process.env
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: "expensify-3bd22.firebaseapp.com",
-  databaseURL: "https://expensify-3bd22.firebaseio.com",
-  projectId: "expensify-3bd22",
-  storageBucket: "expensify-3bd22.appspot.com",
-  messagingSenderId: "387024752558",
-  appId: "1:387024752558:web:a1b034d9753e71f42a823f",
-  measurementId: "G-ZL7M0Y2P5M"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 firebase.initializeApp(firebaseConfig)
--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require('path')
+const webpack = require('webpack')
 // cssを別fileに書き出すためのplugin
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// dotenv用
-const Dotenv = require('dotenv-webpack')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' })
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' })
+}
 
 // objectではなく、関数を指定することによってenv等の引数の使用が可能になる
 module.exports = (env) => {
@@ -10,21 +16,21 @@ module.exports = (env) => {
   const isProduction = env === 'production'
   // mini-css-extract-pluginのfile名等設定
   const MiniCssExtract = new MiniCssExtractPlugin({
-    filename: 'styles.css'
+    filename: 'styles.css',
   })
   return {
     mode: 'development',
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public', 'dist'),
-      filename: 'bundle.js'
+      filename: 'bundle.js',
     },
     module: {
       rules: [
         {
           loader: 'babel-loader',
           test: /\.js$/,
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.s?css$/,
@@ -34,21 +40,47 @@ module.exports = (env) => {
             {
               loader: 'css-loader',
               options: {
-                sourceMap: true
-              }
+                sourceMap: true,
+              },
             },
             {
               loader: 'sass-loader',
               options: {
-                sourceMap: true
-              }
-            }
-          ]
-        }]
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
     },
     plugins: [
       MiniCssExtract,
-      new Dotenv()
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        'process.env.FIREBASE_DATABASE': JSON.stringify(
+          process.env.FIREBASE_DATABASE
+        ),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        ),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(
+          process.env.FIREBASE_MEASUREMENT_ID
+        ),
+      }),
     ],
     // prod時のみsource-mapでsource mapを別fileに
     devtool: isProduction ? 'source-map' : 'inline-source-map',
@@ -58,7 +90,7 @@ module.exports = (env) => {
       // url直打ちで静的sourceが存在しない時404を返されるが、historyApiFallbackをtrueにするとtop urlを返した上でreact-routerによる変遷がなされる
       historyApiFallback: true,
       // bundleのpath
-      publicPath: '/dist/'
-    }
+      publicPath: '/dist/',
+    },
   }
 }
