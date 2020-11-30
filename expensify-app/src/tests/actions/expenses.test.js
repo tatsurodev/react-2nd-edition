06d8eda96f8c9431025b5db8f6cf4b45584bb920
Tatsuro <contacts@tatsuro.dev>
2020-12-01 01:45:21 +0900

    EDIT_EXPENSEの非同期actionを作成、test 160

--- expensify-app/src/tests/actions/expenses.test.js
+++ expensify-app/src/tests/actions/expenses.test.js
@@ -9,6 +9,7 @@ import {
   setExpenses,
   startSetExpenses,
   startRemoveExpense,
+  startEditExpense,
 } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
@@ -64,6 +65,27 @@ test('should setup edit expense action object', () => {
     updates: {
       note: 'New note value',
     },
+  })
+})
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore({})
+  const id = expenses[0].id
+  const updates = { amount: 21045 }
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates,
+    })
+    return database
+      .ref(`expenses/${id}`)
+      .once('value')
+      .then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount)
+        done()
+      })
   })
 })
 
