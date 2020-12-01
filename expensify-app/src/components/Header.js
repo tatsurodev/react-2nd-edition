dc99fab72d944d6ef20a2b0f3dcdbe15245f66a1
Tatsuro <contacts@tatsuro.dev>
2020-12-02 03:51:33 +0900

    public routeでlogin済userがloginへaccess時にdashboardへredirect  167

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({ startLogout }) => (
   <div>
     <h1>Expensify</h1>
     {/* active linkは、activeClassNameでcustomize可能(defaultはactive)、Routeと同じくtoと部分一致する時にもactive classが付与されるので、完全一致させる時はexactを使用する */}
-    <NavLink to="/" activeClassName="is-active" exact={true}>
+    <NavLink to="/dashboard" activeClassName="is-active">
       Dashboard
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
