f18a766c0257d486412c03724b19463414e6f2c6
Tatsuro <contacts@tatsuro.dev>
2020-12-01 17:49:22 +0900

    logout機能追加、test 163

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { startLogout } from '../actions/auth'
 
-const Header = () => (
+export const Header = ({ startLogout }) => (
   <div>
     <h1>Expensify</h1>
     {/* active linkは、activeClassNameでcustomize可能(defaultはactive)、Routeと同じくtoと部分一致する時にもactive classが付与されるので、完全一致させる時はexactを使用する */}
@@ -11,7 +13,12 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create Expense
     </NavLink>
+    <button onClick={startLogout}>Logout</button>
   </div>
 )
 
-export default Header
+const mapDispatchToProps = (dispatch) => ({
+  startLogout: () => dispatch(startLogout()),
+})
+
+export default connect(undefined, mapDispatchToProps)(Header)
