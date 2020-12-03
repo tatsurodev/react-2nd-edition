5ebf9e0c442741c24c7b941fb8f8f51391f29bca
Tatsuro <contacts@tatsuro.dev>
2020-12-04 06:21:23 +0900

    useState実例 192

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -2,27 +2,69 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
-// useStateの引数であるstateをobject形式で使用するpattern
-// 問題点: stateの更新時に、全く新しいstateに置き換わってしまうため、1つのstateのみkey: valueで更新しようとすると、他のkey: valueのstateが失われてしまう。よってhooks使用時はuseStateを何度も使ったほうがわかりやすい
-const App = (props) => {
-  // const [count, setCount] = useState(props.count)
-  // const [text, setText] = useState('')
-  const [state, setState] = useState({
-    count: props.count,
-    text: '',
-  })
+const NoteApp = () => {
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote = e => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      {
+        title,
+        body,
+      },
+    ])
+    setTitle('')
+    setBody('')
+  }
+
+  const removeNote = title => {
+    setNotes(notes.filter(note => note.title !== title))
+  }
+
   return (
     <div>
-      <p>The current {state.text || 'count'} is {state.count}</p>
-      {/* 解決方法 既存のstateをspreadした後に更新する */}
-      {/* <button onClick={() => setState({ ...state, count: state.count - 1 })}>-1</button> */}
-      <button onClick={() => setState({ count: state.count - 1 })}>-1</button>
-      <button onClick={() => setState({ count: props.count })}>Reset</button>
-      <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
-      <input value={state.text} onChange={e => setState({ text: e.target.value })} />
+      <h1>Notes</h1>
+      {notes.map(note => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={e => setTitle(e.target.value)} />
+        <textarea value={body} onChange={e => setBody(e.target.value)}></textarea>
+        <button>Add note</button>
+      </form>
     </div>
   )
 }
+
+// useStateの引数であるstateをobject形式で使用するpattern
+// 問題点: stateの更新時に、全く新しいstateに置き換わってしまうため、1つのstateのみkey: valueで更新しようとすると、他のkey: valueのstateが失われてしまう。よってhooks使用時はuseStateを何度も使ったほうがわかりやすい
+// const App = (props) => {
+//   // const [count, setCount] = useState(props.count)
+//   // const [text, setText] = useState('')
+//   const [state, setState] = useState({
+//     count: props.count,
+//     text: '',
+//   })
+//   return (
+//     <div>
+//       <p>The current {state.text || 'count'} is {state.count}</p>
+//       {/* 解決方法 既存のstateをspreadした後に更新する */}
+//       {/* <button onClick={() => setState({ ...state, count: state.count - 1 })}>-1</button> */}
+//       <button onClick={() => setState({ count: state.count - 1 })}>-1</button>
+//       <button onClick={() => setState({ count: props.count })}>Reset</button>
+//       <button onClick={() => setState({ count: state.count + 1 })}>+1</button>
+//       <input value={state.text} onChange={e => setState({ text: e.target.value })} />
+//     </div>
+//   )
+// }
 
 // useStateの引数であるstateをobject形式でなく、個別に値として使用するpattern
 // stateless functional componentは、state, lifecycle methodが使用できなかったが、hooksにより使用できるようになり、functional componentに名称が変わった
@@ -41,12 +83,12 @@ const App = (props) => {
 //   )
 // }
 
-App.defaultProps = {
-  count: 0,
-}
+// App.defaultProps = {
+//   count: 0,
+// }
 
 ReactDOM.render(
-  <App count={2} />,
+  <NoteApp />,
   document.getElementById('root')
 )
 
