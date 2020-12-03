44c4d18a608e0713f281c5c2017f53e006be447b
Tatsuro <contacts@tatsuro.dev>
2020-12-04 06:17:50 +0900

    useState hooks 190

--- react-new-features/src/index.js
+++ react-new-features/src/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import * as serviceWorker from './serviceWorker'
 
+// stateless functional componentは、state, lifecycle methodが使用できなかったが、hooksにより使用できるようになり、functional componentに名称が変わった
+const App = (props) => {
+  // stateは、class componentの際のような{}の形式でなくてもおｋ
+  const [count, setCount] = useState(props.count)
+
+  return (
+    <div>
+      <p>The current count is {count}</p>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count: 0,
+}
+
 ReactDOM.render(
-  <div>my new content</div>,
+  <App count={2} />,
   document.getElementById('root')
 )
 
