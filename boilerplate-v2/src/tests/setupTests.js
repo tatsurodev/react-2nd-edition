4407b650851c4b4b82de9f8da6443f57ed47e93f
Tatsuro <contacts@tatsuro.dev>
2020-12-04 05:23:19 +0900

    boilerpalte更新 183

---
+++ boilerplate-v2/src/tests/setupTests.js
@@ -0,0 +1,9 @@
+import DotEnv from 'dotenv'
+DotEnv.config({ path: '.env.test' })
+
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({
+  adapter: new Adapter(),
+})
