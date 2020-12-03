e67047788a97cb36cdc0de3e39bdccbce94068a0
Tatsuro <contacts@tatsuro.dev>
2020-12-03 23:39:38 +0900

    ExpenseListのstyling 117

--- expensify-app/src/components/ExpenseList.js
+++ expensify-app/src/components/ExpenseList.js
@@ -5,7 +5,12 @@ import selectExpenses from '../selectors/expenses'
 
 // stateとunconnectedな状態でtestする必要があるのでexport
 export const ExpenseList = (props) => (
-  <div>
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
     {props.expenses.length === 0 ? (
       <p>No expenses</p>
     ) : (
