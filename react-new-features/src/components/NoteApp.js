839e8039230e9b417511086759be5457c67036c7
Tatsuro <contacts@tatsuro.dev>
2020-12-04 06:39:21 +0900

    fragment 199

--- react-new-features/src/components/NoteApp.js
+++ react-new-features/src/components/NoteApp.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import notesReducer from '../reducers/notes'
 import NoteList from './NoteList'
 import AddNoteForm from './AddNoteForm'
